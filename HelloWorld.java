de1f87ff5275dd2b66f8d9c08a84e760fc82cfde
AmruthReddy20 <muthyalaamruthreddy@gmail.com>
2022-01-18 18:44:45 +0530

    SOLID_PRINCIPLES_BEFORE-MODIFICATION

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,21 @@
 package General;
 
 public class HelloWorld {
+    String str;
     public static void main(String[] args) {
-
-        System.out.println("HelloWorld");
-        System.out.println("Hello Amruth");
+        HelloWorld h= new HelloWorld();
+       method(str); // non-static variable str cannot be referenced from a static context
+        h.print();
     }
+
+    private static HelloWorld method(String str) {
+        str="abc";
+    return new HelloWorld();
+    }
+
+    public  void print() {
+        System.out.println("String :"+str);
+
+    }
+
 }
