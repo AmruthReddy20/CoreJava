219a35055ca72de00dcd9973e9f1eba9deae854e
AmruthReddy20 <muthyalaamruthreddy@gmail.com>
2022-01-18 18:51:31 +0530

    name and age

---
+++ VariableFun.java
@@ -0,0 +1,9 @@
+package General;
+
+public class VariableFun {
+    public static void main(String[] args) {
+        int age=21;
+        String name="Amruth";
+        System.out.println("My name is "+name+"\n and age is :"+age);
+    }
+}
