4f0dd24c6038e9651f3e5d6be129918061f27957
AmruthReddy20 <muthyalaamruthreddy@gmail.com>
2022-01-18 18:44:45 +0530

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package General;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("HelloWorld");
+        System.out.println("Hello Amruth");
+    }
+}
